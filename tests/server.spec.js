36f3470574dd16e241bff5d8c6c91df0f89e053e
Pablo Goeppinger <pgoeppin@gmail.com>
2023-01-30 20:46:33 -0300

    Adding tests in GET and DELETE

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,37 @@
 const request = require("supertest");
 const server = require("../index");
+const { faker } = require("@faker-js/faker");
 
 describe("Operaciones CRUD de cafes", () => {
-
+  describe("GET /cafes", () => {
+    it("Obteniendo un 200", async () => {
+      const response = await request(server).get("/cafes").send();
+      const status = response.statusCode;
+      expect(status).toBe(200);
+    });
+    it("Debiese obtener un array", async () => {
+      const response = await request(server).get("/cafes").send();
+      expect(response.body).toBeInstanceOf(Array);
+    });
+    it("El array no esta vacio", async () => {
+      const response = await request(server).get("/cafes").send();
+      expect(response.body.length).not.toEqual(0);
+    });
+    it("El array solo debe contener objetos", async () => {
+      const response = await request(server).get("/cafes").send();
+      response.body.map((element) => expect(element).toBeInstanceOf(Object));
+    });
+  });
+  describe("DELETE /cafes/:id", () => {
+    it("Eliminando un producto que no existe que devuelva 404", async () => {
+      const jwt = "token";
+      const idCafeAEliminar = 5;
+      const response = await request(server)
+        .delete(`/cafes/${idCafeAEliminar}`)
+        .set("Authorization", jwt)
+        .send();
+      const status = response.statusCode;
+      expect(status).toBe(404)
+    });
+  });
 });
