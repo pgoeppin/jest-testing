85bf2371969e5a71448c0c17e46ebf4c6acbcd99
Pablo Goeppinger <pgoeppin@gmail.com>
2023-01-30 21:43:47 -0300

    PUT tests

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -23,7 +23,7 @@ describe("Operaciones CRUD de cafes", () => {
     });
   });
   describe("DELETE /cafes/:id", () => {
-    it("Eliminando un producto que no exista el id y que devuelva 404", async () => {
+    it("Eliminando un cafe que no exista el id y que devuelva 404", async () => {
       const jwt = "token";
       const idCafeAEliminar = 5;
       const response = await request(server)
@@ -31,18 +31,48 @@ describe("Operaciones CRUD de cafes", () => {
         .set("Authorization", jwt)
         .send();
       const status = response.statusCode;
-      expect(status).toBe(404)
+      expect(status).toBe(404);
+    });
+    it("Eliminar un cafe sin autorizacion(token)", async () => {
+      const idCafeAEliminar = 4;
+      const response = await request(server)
+        .delete(`/cafes/${idCafeAEliminar}`)
+        .send();
+      const status = response.statusCode;
+      expect(status).toBe(400);
     });
   });
   describe("POST /cafes", () => {
-    const nuevoCafe = {
-        id: Math.floor(Math.random() * 999),
-        nombre: faker.helpers.arrayElement(['Macchiato', 'Frappuchino', 'Mocachino'])
-    }
     it("Al crear un nuevo cafe que devuelva un codigo 201", async () => {
-        const response = await request(server).post("/cafes").send(nuevoCafe)
-        const status = response.statusCode
-        expect(status).toBe(201);
-    })
-  })
+      const nuevoCafe = {
+        id: Math.floor(Math.random() * 999),
+        nombre: faker.helpers.arrayElement([
+          "Macchiato",
+          "Frappuchino",
+          "Mocachino",
+        ]),
+      };
+      const response = await request(server).post("/cafes").send(nuevoCafe);
+      const status = response.statusCode;
+      expect(status).toBe(201);
+    });
+  });
+  describe("PUT /cafes/:id", () => {
+    it("Id cafe en parametros distinto a id cafe en el payload devuelve un codigo 400", async () => {
+      const idCafeAModificar = 2;
+      const cafeAModificar = {
+        id: 1,
+        nombre: faker.helpers.arrayElement([
+          "Macchiato",
+          "Frappuchino",
+          "Mocachino",
+        ]),
+      };
+      const response = await request(server)
+        .put(`/cafes/${idCafeAModificar}`)
+        .send(cafeAModificar);
+      const status = response.statusCode;
+      expect(status).toBe(400);
+    });
+  });
 });
